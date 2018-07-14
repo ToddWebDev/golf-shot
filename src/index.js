8f9095d41c9f8ba25860e53b6f9ff665c5983e5f
ToddWebDev <webdevtodd@gmail.com>
2018-07-14 10:10:39 -0500

    Init App Structure and Setup Router

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log('Hello, world.');
+/*eslint-disable import/default */
+import 'babel-polyfill';
+import React from 'react';
+import { render } from 'react-dom';
+import { Router, browserHistory } from 'react-router';
+import routes from './routes';
+import './styles/styles.css'; //Webpack can import CSS files too!
+
+render(
+  <Router history={browserHistory} routes={routes} />,
+  document.getElementById('app')
+);
