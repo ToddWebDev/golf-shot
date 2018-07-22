54fce9e239b24cbe5009912af33ee2072f4c5c0f
ToddWebDev <webdevtodd@gmail.com>
2018-07-22 17:05:59 -0500

    Show # Courses in Header

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -2,12 +2,12 @@ import React, {PropTypes} from 'react';
 import { Link, IndexLink } from 'react-router';
 import LoadingDots from './LoadingDots';
 
-const Header = ({ loading }) => {
+const Header = ({ loading, courses }) => {
   return (
     <nav>
       <IndexLink to="/" activeClassName="active">Home</IndexLink>
       {" | "}
-      <Link to="/courses" activeClassName="active">Courses</Link>
+      <Link to="/courses" activeClassName="active">Courses <span className="badge badge-primary">{courses.length}</span></Link>
       {" | "}
       <Link to="/scores" activeClassName="active">Scores</Link>
       {" | "}
@@ -18,13 +18,14 @@ const Header = ({ loading }) => {
 };
 
 Header.propTypes = {
-  children: PropTypes.object.isRequired,
-  loading: PropTypes.bool.isRequired
+  loading: PropTypes.bool.isRequired,
+  courses: PropTypes.array.isRequired
 };
 
 function mapStateToProps(state, ownProps) {
   return {
-    loading: state.ajaxCallsInProgress > 0
+    loading: state.ajaxCallsInProgress > 0,
+    courses: state.courses
   };
 }
 
