ef70091cb3bd55d16c151fb57a0b87535369e8a5
ToddWebDev <webdevtodd@gmail.com>
2018-07-22 19:24:34 -0500

    Update Header Component

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -4,14 +4,11 @@ import LoadingDots from './LoadingDots';
 
 const Header = ({ loading, courses }) => {
   return (
-    <nav>
-      <IndexLink to="/" activeClassName="active">Home</IndexLink>
-      {" | "}
-      <Link to="/courses" activeClassName="active">Courses <span className="badge badge-primary">{courses.length}</span></Link>
-      {" | "}
-      <Link to="/scores" activeClassName="active">Scores</Link>
-      {" | "}
-      <Link to="/about" activeClassName="active">About</Link>
+    <nav className="nav">
+      <IndexLink to="/" className="nav-link" activeClassName="active">Home</IndexLink>
+      <Link to="/courses" className="nav-link" activeClassName="active">Courses <span className="badge badge-primary">{courses.length}</span></Link>
+      <Link to="/scores" className="nav-link" activeClassName="active">Scores</Link>
+      <Link to="/about" className="nav-link" activeClassName="active">About</Link>
       {loading && <LoadingDots interval={100} dots={5} />}
     </nav>
   );
